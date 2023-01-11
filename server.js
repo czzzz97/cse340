5ca0f3dcc9a3a71c2e62ed855599851f4ec0d6e0
Corbin Hughes <hug15006@byui.edu>
2023-01-10 21:24:11 -0700

    EJS files started

--- server.js
+++ server.js
@@ -6,13 +6,17 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 
 /* ***********************
- * Routes
+ * Routes ALSO WHAT "MULTI-LINE ROUTES COMMENT" am I copying??
  *************************/
 app.use(require("./routes/static"))
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Local Server Information
