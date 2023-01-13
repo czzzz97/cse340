7f578eeb5795daf01ff91eba63ef841cbc52db0d
Corbin Hughes <hug15006@byui.edu>
2023-01-13 11:28:05 -0700

    Fixed order of items in server.js

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 
+/********
+ *  View Engine and Templates
+ ********/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
 /* ***********************
  * Routes 
  *************************/
@@ -18,12 +25,6 @@ app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 })
 
-/********
- *  View Engine and Templates
- ********/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Local Server Information
