5494a68ae2938e49c4b2cbcb6b23667f2da1c23b
Corbin Hughes <hug15006@byui.edu>
2023-02-01 11:08:57 -0700

    Setup for MVC

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+const baseController = require("./controllers/baseController")
 
 /********
  *  View Engine and Templates
@@ -21,9 +22,7 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes 
  *************************/
 app.use(require("./routes/static"))
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
 
 
 /* ***********************
