530b0c0181a72d6afebced8d645254102eca2dbe
Corbin Hughes <hug15006@byui.edu>
2023-02-10 13:13:28 -0700

    Finally working. Style isn't loaded tho

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -4,4 +4,4 @@ async function getClassifications(){
     return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
 }
 
-module.exports = (getClassifications)
+module.exports = {getClassifications}
