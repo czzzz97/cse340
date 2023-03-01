855efaf8b684fcf8d549cd95c5c213415e69e651
Corbin Hughes <hug15006@byui.edu>
2023-02-28 17:51:44 -0700

    Activities done, testing time

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,7 @@
 utilities = require("../utilities/index")
 accountModel = require("../models/account-model")
+const bcrypt = require("bcryptjs")
+
 
 async function buildLogin(req, res, next) {
     let nav = await utilities.getNav()
@@ -23,13 +25,29 @@ async function registerClient(req, res) {
     let nav = await utilities.getNav()
     const { client_firstname, client_lastname, client_email, client_password } =
       req.body
-  
+
+      let hashedPassword
+      try {
+        // pass regular password and cost (salt is generated automatically)
+        hashedPassword = await bcrypt.hashSync(client_password, 10)
+      } catch (error) {
+        res.status(500).render("clients/register", {
+          title: "Registration",
+          nav,
+          message: 'Sorry, there was an error processing the registration.',
+          errors: null,
+        })
+      }
+
     const regResult = await accountModel.registerClient(
       client_firstname,
       client_lastname,
       client_email,
-      client_password
+      hashedPassword
     )
+
+
+    
     console.log(regResult)
     if (regResult) {
       res.status(201).render("./clients/login-view.ejs", {
--- routes/account-route.js
+++ routes/account-route.js
@@ -1,10 +1,23 @@
+const regValidate = require('../utilities/account-validation')
 const express = require("express"); 
 const router = new express.Router(); 
 const util = require("../utilities/index")
 const accountController = require("../controllers/accountController");
 
 router.get("/login", accountController.buildLogin);
+router.post(
+  "/login",
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
+
 router.get("/register", accountController.buildRegister);
-router.post("/register", accountController.registerClient);
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    accountController.registerClient
+  )
 
 module.exports = router;
